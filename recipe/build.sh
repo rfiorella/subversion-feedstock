dce45a76e653822e29bf00ee1cdc491410c4ddbe
Rich Fiorella <rich.p.fiorella@gmail.com>
2025-12-25 06:05:00 -0700

    add additional perl env variable

--- recipe/build.sh
+++ recipe/build.sh
@@ -19,14 +19,18 @@ export CFLAGS="${CFLAGS} -U__USE_XOPEN2K -std=c99"
   "$@"
 
 
+# Ensure Perl can find the modules
+if [ -d "${PREFIX}/lib/perl5" ]; then
+    export PERL5LIB="${PREFIX}/lib/perl5:${PERL5LIB:-}"
+fi
+
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
     make -j ${CPU_COUNT} check CLEANUP=true TESTS=subversion/tests/cmdline/basic_tests.py
 else
-    make -j ${CPI_COUNT}
+    make -j ${CPU_COUNT}
 fi
 make install
 
 make swig-pl-lib
 make install-swig-pl-lib
-
 
